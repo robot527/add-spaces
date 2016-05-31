50687586eb4ae0dc07c2d5966000911119f5ab44
robot527 <luoyaozu@foxmail.com>
2016-05-31 22:59:28 +0800

    新增README并改进主程序
    新文件:       README.md
    修改:         add_spaces.py

--- add_spaces.py
+++ add_spaces.py
@@ -25,9 +25,25 @@ def isalpha(uni_ch):
         return False
 
 
+def is_e_leftbracket(uni_ch):
+    """判断一个 unicode 是否英文左括号。"""
+    if uni_ch == u'(' or uni_ch == u'[':
+        return True
+    else:
+        return False
+
+
 def is_e_rightbracket(uni_ch):
     """判断一个 unicode 是否英文右括号。"""
-    if uni_ch == u')':
+    if uni_ch == u')' or uni_ch == u']':
+        return True
+    else:
+        return False
+
+
+def is_c_leftbracket(uni_ch):
+    """判断一个 unicode 是否中文左括号。"""
+    if uni_ch == u'\uff08':
         return True
     else:
         return False
@@ -42,26 +58,35 @@ def is_c_rightbracket(uni_ch):
 
 
 def add_spaces_to_ustring(ustr):
+    """给 unicode 字符串添加合理的空格。"""
     newstring = ""
     char_list = list(ustr)
-    #print char_list
-    for i in range(0, len(char_list)):
-        if i < len(char_list) - 1:
-            if (is_chinese(char_list[i]) and isdigit(char_list[i + 1])) or (is_chinese(char_list[i]) and isalpha(char_list[i + 1])):
-                char_list[i] = char_list[i]+" "
-            elif (isdigit(ustr[i]) and is_chinese(ustr[i + 1])):
-                char_list[i] = char_list[i]+" "
-            elif (isalpha(ustr[i]) and is_chinese(ustr[i + 1])):
-                char_list[i] = char_list[i]+" "
-            elif (is_e_rightbracket(ustr[i]) and is_chinese(ustr[i + 1])):
-                char_list[i] = char_list[i]+" "
-            elif (is_c_rightbracket(ustr[i]) and isdigit(ustr[i + 1])) or (is_c_rightbracket(ustr[i]) and isalpha(ustr[i + 1])):
-                char_list[i] = char_list[i]+" "
+    length = len(char_list)
+    for i in range(0, length):
+        if i < length - 1:
+            #中文(括号)与英文(括号)之间需要增加空格
+            if (is_chinese(char_list[i]) and isalpha(char_list[i + 1]))\
+                or (isalpha(char_list[i]) and is_chinese(char_list[i + 1])):
+                char_list[i] += u" "
+            elif (isalpha(char_list[i]) and is_c_leftbracket(char_list[i + 1]))\
+                or (is_c_rightbracket(char_list[i]) and isalpha(char_list[i + 1])):
+                char_list[i] += u" "
+            elif (is_chinese(char_list[i]) and is_e_leftbracket(char_list[i + 1]))\
+                or (is_e_rightbracket(char_list[i]) and is_chinese(char_list[i + 1])):
+                char_list[i] += u" "
+            #中文(括号)与数字之间需要增加空格
+            if (is_chinese(char_list[i]) and isdigit(char_list[i + 1]))\
+                or (isdigit(char_list[i]) and is_chinese(char_list[i + 1])):
+                char_list[i] += u" "
+            elif (isdigit(char_list[i]) and is_c_leftbracket(char_list[i + 1]))\
+                or (is_c_rightbracket(char_list[i]) and isdigit(char_list[i + 1])):
+                char_list[i] += u" "
 
         newstring = newstring + char_list[i]
     return newstring
 
 def add_spaces_to_file(file_name):
+    """给文本文件添加合理的空格。"""
     try:
         with open(file_name, 'r+') as text:
             #line_list = [add_spaces_to_ustring(unicode(line.rstrip(), "UTF-8")).encode('UTF-8') + '\n' for line in text]
@@ -72,6 +97,7 @@ def add_spaces_to_file(file_name):
             print 'Finished adding spaces.'
     except IOError as err:
         print 'File error: ' + str(err)
+
 
 if __name__ == '__main__':
     print 'Please input a file name.'
